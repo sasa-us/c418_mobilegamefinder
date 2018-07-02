c75bea56e366a0b24560f9a16ce987a839a692c6
Steffany <kielen@gmail.com>
2018-07-01 17:04:13 -0700

    integrated carousel into home page, created generic text component for future use

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Carousel from '../carousel';
+import './home.scss';
+import GeneralText from '../multiuse/generaltext';
 
 
 
 export default props => {
     return (
-        <div>
+        <div className="homePageContainer">
             <h1>Home Page</h1>
+            <GeneralText />
+            <Carousel />
+        
         </div>
         
     )
---
+++ client/src/components/multiuse/generaltext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './generaluse.scss';
+
+export default props => {
+    return(
+        <div className="generalTextContainer">
+            <p>
+                General text area. We would have basic info about what the page is for, etc. here. 
+            </p>
+        </div>
+    )
+}
