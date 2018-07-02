e7df9c1d175a1f816a95f9f06507376a1bcb665a
delray <d.a.faskey@gmail.com>
2018-07-01 17:13:00 -0700

    created main menu icon animation

--- client/src/components/header-bar.js
+++ client/src/components/header-bar.js
@@ -68,9 +68,20 @@ class HeaderBar extends Component {
             background: this.state.dropdownsOpen.main ? "rgba(0,0,0,.5)" : "rgba(0,0,0,0)",
             "pointer-events": this.state.dropdownsOpen.main ? "auto" : "none"
         };
-        const mainMenuButtonStyle = {
+        // -----------------------------------
+        const mainMenuButtonPosition = {
             transform: this.state.dropdownsOpen.main ? "translateX(75px)" : "translateX(0px)"
         };
+        const hamburgerXdiv2 = {
+            transform: this.state.dropdownsOpen.main ? "rotateZ(-45deg)" : "initial"
+        };
+        const hamburgerXdiv3 = {
+            transform: this.state.dropdownsOpen.main ? "rotateZ(45deg)" : "initial"
+        };
+        const hamburgerXdivFade = {
+            opacity: this.state.dropdownsOpen.main ? "0" : "1"
+        }
+        // -----------------------------------
         const mainMenuStyle = {
             width: this.state.dropdownsOpen.main ? "36%" : "0"
         };
@@ -90,14 +101,22 @@ class HeaderBar extends Component {
             <div>
                 <nav className="header">
                     <div className="menu-overlay" onClick={this.toggleMainMenu.bind(this)} style={menuOverlayStyle}></div>
-                    <img className="menu-icon" src={MenuIcon} alt="menu icon" aria-label="Menu" onClick={this.toggleMainMenu.bind(this)} style={mainMenuButtonStyle}/>
+                    <button className="menu-icon" type="button" onClick={this.toggleMainMenu.bind(this)} style={mainMenuButtonPosition} aria-expanded={`${this.state.dropdownsOpen.main ? "true" : "false"}`} aria-label="Toggle main navigation menu">
+                        <div className="hamburger-animation">
+                            <div style={hamburgerXdivFade}></div>
+                            <div style={hamburgerXdiv2}></div>
+                            <div style={hamburgerXdiv3}></div>
+                            <div style={hamburgerXdivFade}></div>
+                        </div>
+                    </button>
+                    {/* <img className="menu-icon" src={MenuIcon} alt="menu icon" aria-label="Menu" /> */}
                     <ul className="main-menu" style={mainMenuStyle}>
                         <Link to="/"><li className="nav-text">Home</li></Link>
                         <Link to="/search"><li className="nav-text">Search</li></Link>
                         <li className="nav-text">
                             <div className="plus-bar" onClick={this.toggleBrowseMenu.bind(this)}>
                                 <Link to="/browse">Browse </Link>
-                                <span className={`glyphicon glyphicon-${this.state.dropdownsOpen.browse ? "minus" : "plus"}`} alt="list expand icon" aria-label="Browse-Menu expansion"></span>
+                                <span className={`glyphicon glyphicon-${this.state.dropdownsOpen.browse ? "minus" : "plus"}`} alt="list expand icon" aria-expanded={`${this.state.dropdownsOpen.browse ? "true" : "false"}`} aria-label="Browse-Menu expansion"></span>
                             </div>
                             <ul className="browse-menu" style={browseMenuStyle}>
                                 <li><a>By Popularity</a></li>
@@ -110,7 +129,7 @@ class HeaderBar extends Component {
                         <li className="nav-text nav-text-bottom">
                             <div className="plus-bar" onClick={this.toggleAboutMenu.bind(this)}>
                                 <Link to="/about">About </Link>
-                                <span className={`glyphicon glyphicon-${this.state.dropdownsOpen.about ? "minus" : "plus"}`} alt="list expand icon" aria-label="About-Menu expansion"></span>
+                                <span className={`glyphicon glyphicon-${this.state.dropdownsOpen.about ? "minus" : "plus"}`} alt="list expand icon" aria-expanded={`${this.state.dropdownsOpen.about ? "true" : "false"}`} aria-label="About-Menu expansion"></span>
                             </div>
                             <ul className="about-menu" style={aboutMenuStyle}>
                                 <li><a>Contact Us</a></li>
