57efa59c40d8e6c7bdc1e9c05c4b394af7638a28
sasa-us <sasa_luu@hotmail.com>
2018-07-07 17:20:55 -0700

    organize wizard to post folder, make get main page and post wizard point to same php

--- public/api/ajaxcallmainpage.php
+++ public/api/ajaxcallmainpage.php
@@ -2,8 +2,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
 <script>
 
-   // axios.get('mainpage.php', {
-    axios.get('gameapp.php', {
+   axios.get('mainpage.php', {
+    //axios.get('gameapp.php', {
         params: {
             action: 'get_mainpage'
         }
--- public/api/mainpage.php
+++ public/api/mainpage.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 header('Access-Control-Allow-Origin: *');
 
 if(empty($_GET['action'])){
@@ -18,33 +18,14 @@ switch($_GET['action']) {
 		include 'get/get_mainpage.php';
         break;
 
-    // case 'get_detailspage':
-    //     include 'get/get_detailspage.php';
-    //     break;
-
-    // case 'get_searchpage';
-    //     include 'get/get_searchpage.php';
-    //     break;
-
+    
     default: 
         $output['error'] ="unknown request method: $method";
 
 }
 //find which request front end used
-//$method = $_SERVER['REQUEST_METHOD'];//GET
 
-//$action = $_GET['action'];//
-//print_r($action);get-mainpage
-
-// switch($method) {
-//     case 'GET': 
-//           include "get/$action.php";      
-//           break;
-
-//     default: 
-//           $output['error'] ="unknown request method: $method";
-// }
 
 $output_json = json_encode($output);
 print($output_json);
-?> -->
+?> 
