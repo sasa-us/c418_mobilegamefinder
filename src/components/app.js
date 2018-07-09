f0b3439d8854dea48241dd77377fd2ca65223106
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-07-09 15:40:45 -0700

    added navigation between wizard pages and saving data about price, platform and genre for future query string. No data calls yet

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import {Route} from 'react-router-dom';
 import Header from './header-bar';
 import Home from './home';
 import Browse from './browse';
-import Wizard from './wizard/wizard2';
+import Wizard from './wizard/price';
+import Platform from "./wizard/platform";
+import Genre from "./wizard/genre";
 import Search from './search';
 import GameInfo from '../components/gamedetails'
 import About from './about';
@@ -21,6 +23,8 @@ const App = () => (
             <Route exact path="/" component={Home}/>
             <Route path="/browse" component={Browse}/>
             <Route path="/wizard" component={Wizard}/>
+            <Route path="/platform" component={Platform}/>
+                <Route path="/genre" component={Genre}/>
             <Route exact path="/search" component={Search}/>
             <Route path="/about" component={About}/>
             <Route path="/recommendations" component={Recommendations}/>
---
+++ src/components/wizard/price.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom"
+import Paid from "../../assets/images/wizard-icons/001-money.png";
+import Free from "../../assets/images/wizard-icons/002-free.png";
+import "./wizard.scss";
+
+
+
+class Wizard extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            price:"",
+
+    };
+        this.handleIconClick = this.handleIconClick.bind(this);
+    }
+
+    handleIconClick(e, title) {
+        let price = {
+            price: title
+        };
+        this.setState(price);
+        localStorage.setItem('price', JSON.stringify(price));
+        console.log(localStorage);
+    }
+
+    render() {
+
+        return(
+
+            <div className="wizardContainer">
+
+                <h4 className="priceGameName">Are you looking for FREE or PAID games?</h4>
+
+
+                <div className="priceIcons">
+
+                    <div className="flexColumn">
+                        <div className="icon">
+                            <img className="game_wiz" src ={Free} onClick={(e) => this.handleIconClick(e, 'free')} />
+                        </div>
+
+                        <div className="icon">
+                            <img className="game_wiz" src ={Paid} onClick={(e) => this.handleIconClick(e, 'paid')}/>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="buttonContainer">
+                    <Link to="/platform">
+                        <button className="wizard-button" type="button">Next</button>
+                    </Link>
+                </div>
+            </div>
+
+
+        )
+    }
+
+}
+export default Wizard;
