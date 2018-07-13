3f2a7849f9280d3990d2073f233ed91cf4635376
Steffany <kielen@gmail.com>
2018-07-12 19:54:35 -0700

    updated search functionality, but need to merge dev in to complete testing

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Results from "../results"
 import { formatPostData} from '../../helpers'
 
 
-export default props => {
-    const pageText = 'Here are your search results';
-    return (
-        <div>
-            <Results title='Search Results' search="angry birds" text={pageText}/>
-        </div>
-        
-    )
+class SearchResults extends Component {
+
+    componentDidMount(){
+        const search_term = new URLSearchParams(this.props.location.search).get('search_term');
+    }
+    componentWillReceiveProps(newProps){
+        const search_term = new URLSearchParams(newProps.location.search).get('search_term');
+    }
+
+    render(){
+        const pageText = 'Here are your search results';
+        return (
+            <div>
+                <Results title='Search Results' search={this.search_term} text={pageText}/>
+            </div>
+        )
+    }
 }
+export default SearchResults;
