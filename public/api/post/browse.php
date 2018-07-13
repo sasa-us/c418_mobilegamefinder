880c44c1d165e7fbe4b42f125d6c3cd58458e4fe
sasa-us <sasa_luu@hotmail.com>
2018-07-12 23:24:11 -0700

    browse by rating done

--- public/api/post/browse.php
+++ public/api/post/browse.php
@@ -1,30 +1,39 @@
 <?php
+$all_rating = $_POST['all_rating'];
 $genre = $_POST['genre']; 
-$platform = $_POST['platform'];
 $price_value = $_POST['price_value'];
 
-if(empty($output['error'])) {
-    $query = ("SELECT * FROM `combined_game_content`
-        WHERE `price_value` = '$price_value'
-        OR `genre` = '$genre'
-        OR `platform` = '$platform'
-        ORDER BY RAND ()
-        LIMIT 25");
+//browse rating: will send back `all_rating`=5 & 27pc
+if($all_rating) {
+    $query = "SELECT `genre`, `app_name`, `icon_url`, `game_id`, `all_rating` 
+                FROM `combined_game_content` 
+                WHERE `all_rating`=5 
+                LIMIT 27";
+     getBrowseData($conn, $query);
+} else if($genre) {
+    $query = "";
+}
 
+
+
+function getBrowseData($conn, $query) {
     $result = mysqli_query($conn, $query);
+    global $output;
     if(empty($result)) {
-    $output['error'][] = mysqli_error($conn);
+        $output['error'][] = mysqli_error($conn);
     } else {
         if(mysqli_num_rows($result) > 0) {
             $output['success'] = true;
             while($row = mysqli_fetch_assoc($result)) {
                 $output['data'][] = $row;
-            }
-        
+             }
         } else {
             $output['error'][] = 'no result';
         }
     }
-}
+} //end getBrowseData() 
+
+
+
 
 ?>
