29426752f96bb993284ec56443388f53328136d1
Daniel S <danielksstone@gmail.com>
2018-07-15 19:58:06 -0700

    added platform and description

--- public/api/post/browse.php
+++ public/api/post/browse.php
@@ -1,7 +1,7 @@
 <?php
 //browse rating: will send back `all_rating`=5 & 27pc
 if(isset($_POST['all_rating'])) {
-    $query = "SELECT `all_rating`, `genre`, `app_name`, `icon_url`, `game_id`, `price_value`
+    $query = "SELECT `all_rating`, `genre`, `app_name`, `icon_url`, `game_id`, `price_value`, `description`, `platform`
                 FROM `combined_game_content` 
                 WHERE `all_rating`=5 
                 ORDER BY RAND()
@@ -11,7 +11,7 @@ if(isset($_POST['all_rating'])) {
 
 //==============================  genre  =========================================
 else if(isset($_POST['genre'])) {
-    $query = "SELECT `genre`, `app_name`, `icon_url`, `game_id`, `all_rating`, `price_value` 
+    $query = "SELECT `genre`, `app_name`, `icon_url`, `game_id`, `all_rating`, `price_value`, `description`, `platform`
                 FROM `combined_game_content`
                 WHERE `genre` = '{$_POST['genre']}'
                 ORDER BY RAND()
@@ -19,16 +19,26 @@ else if(isset($_POST['genre'])) {
     getBrowseData($conn, $query);
    
 }//end if($genre)
+//==============================  platform  =========================================
+else if(isset($_POST['platform'])) {
+    $query = "SELECT `genre`, `app_name`, `icon_url`, `game_id`, `all_rating`, `price_value`, `description`, `platform`
+                FROM `combined_game_content`
+                WHERE `platform` = '{$_POST['platform']}'
+                ORDER BY RAND()
+                LIMIT 27";
+    getBrowseData($conn, $query);
+   
+}//end if($platform)
 //==============================  price_value  ==================================
 else if(isset($_POST['price_value'])) {
     if($_POST['price_value']=='paid') {
-        $query = "SELECT `price_value`, `genre`, `app_name`, `icon_url`, `game_id`, `all_rating` 
+        $query = "SELECT `price_value`, `genre`, `app_name`, `icon_url`, `game_id`, `all_rating`, `description`, `platform`
                 FROM `combined_game_content`
                 WHERE `price_value` <> 'free'
                 ORDER BY RAND()
                 LIMIT 27";
     } else {
-        $query = "SELECT `price_value`, `genre`, `app_name`, `icon_url`, `game_id`, `all_rating` 
+        $query = "SELECT `price_value`, `genre`, `app_name`, `icon_url`, `game_id`, `all_rating`, `description` , `platform`
                 FROM `combined_game_content`
                 WHERE `price_value` = 'free'
                 ORDER BY RAND()
