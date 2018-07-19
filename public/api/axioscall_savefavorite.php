ed58e4916f621a731b143a3f5d8eb7f33739ca49
sasa-us <sasa_luu@hotmail.com>
2018-07-19 16:55:45 -0700

    modify favorate page to single app

--- public/api/axioscall_savefavorite.php
+++ public/api/axioscall_savefavorite.php
@@ -1,15 +1,16 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
 <script>
 
-let recommendedGame = ['com.gameloft.android.ANMP.GloftINHM',           
-                        'techmasterplus.classicsudokupro', 'techmasterplus.sudokumasteradfree'
-                      ];
+// let recommendedGame = ['com.gameloft.android.ANMP.GloftINHM',           
+//                         // 'techmasterplus.classicsudokupro', 'techmasterplus.sudokumasteradfree'
+//                       ];
 
   
-recommendedGame = JSON.stringify(recommendedGame);
+// recommendedGame = JSON.stringify(recommendedGame);
 const newItem = {
         user_id: '1',
-        favorite_list: recommendedGame
+        game_id: 'tasas.classicsudokupro'
+        //favorite_list: recommendedGame
     };
 const postnewItem = formatPostData(newItem);
 axios.post('/api/gameapp.php', postnewItem, {
